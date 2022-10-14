8f586c1294a7e58d6530a350b72dfc5f25879f17
Madicken Eriksson <conlerc@live.se>
2022-10-14 12:15:35 +0200

    validate started

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,25 @@
-const validate (e) => (
-    
-)
+
+const checkLength = (element,errorMessage = "This field is required", minLength =2) => {
+    console.log(element.target.id)
+    if (element.target.value.length < minLength) {
+        document.getElementById(element.target.id).classList.add('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = "errorMessage"
+    } else {
+        document.getElementById(element.target.id).classList.remove('error')
+        document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
+
+    }
+}
+
+const validate = (e) => {
+    switch(e.target.type) {
+        case "text":
+            checkLength(e)
+            break;
+        case "email":
+            break;
+        case "textarea":
+            checkLength(e, 5)
+            break;
+    }
+}
