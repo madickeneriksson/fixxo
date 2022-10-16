1454707fb0d47e4b6fd5dc7a8fdc3b28a687b238
Madicken Eriksson <conlerc@live.se>
2022-10-16 15:31:36 +0200

    validate

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,11 @@
+const checkLength = (element, minLength = 2, message) => {
 
-const checkLength = (element,errorMessage = "This field is required", minLength =2) => {
-    console.log(element.target.id)
+    if (message === undefined)
+    message = `Your ${element.target.id} must contain at least ${minLength} characters`
+
     if (element.target.value.length < minLength) {
         document.getElementById(element.target.id).classList.add('error')
-        document.getElementById(`${element.target.id}ErrorMessage`).innerText = "errorMessage"
+        document.getElementById(`${element.target.id}ErrorMessage`).innerHTML = message
     } else {
         document.getElementById(element.target.id).classList.remove('error')
         document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
@@ -11,12 +13,27 @@ const checkLength = (element,errorMessage = "This field is required", minLength 
     }
 }
 
+const checkEmail = (element, message) => {
+    if (message === undefined)
+    message = `Your ${element.target.id} must be a valid e-mail address`
+
+    if (!element.target.value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+    document.getElementById(element.target.id).classList.add('error')
+    document.getElementById(`${element.target.id}ErrorMessage`).innerHTML = message
+    } else {
+    document.getElementById(element.target.id).classList.remove('error')
+    document.getElementById(`${element.target.id}ErrorMessage`).innerText = ""
+}
+}
+
+
 const validate = (e) => {
     switch(e.target.type) {
         case "text":
             checkLength(e)
             break;
         case "email":
+            checkEmail (e)
             break;
         case "textarea":
             checkLength(e, 5)
